41b11a00c45bee66b07084a07e57e22dd2c17df9
Yuri <wall.mart.boss@proton.me>
2024-06-29 01:43:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import Profile from "./Profile/Profile";
-import userData from "../userData.json";
+import userData from "/src/userData.json";
 import FriendList from "./FriendList/FriendList"
-import friends from "../friends.json/"
-import transactions from "../transactions.json"
+import friends from "/src/friends.json/"
+import transactions from "/src/transactions.json"
 import TransactionHistory from "./TransactionHistory/TransactionHistory"
 
 const App = () => {
