3c2ce9c328a25a56afb58c6f1e5999b10535624a
Yuri <wall.mart.boss@proton.me>
2024-06-29 01:53:04 +0300

    Updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import Profile from "./Profile/Profile";
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import userData from "/src/userData.json";
-import FriendList from "./FriendList/FriendList"
-import friends from "/src/friends.json/"
-import transactions from "/src/transactions.json"
-import TransactionHistory from "./TransactionHistory/TransactionHistory"
+import friends from "/src/friends.json/";
+import transactions from "/src/transactions.json";
+
 
 const App = () => {
   return (
